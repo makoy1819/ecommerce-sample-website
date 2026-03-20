67d61b260db8b50439ed10f6eeee7be39889067e
raymarkcanero <raymarkcanero21@gmail.com>
2026-03-20 20:01:00 +0800

    Import products from product.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 //Import to use the variable outside of file
 import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
 let productsHTML = '';
 
 products.forEach((product) => {
