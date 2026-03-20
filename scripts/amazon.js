053d56cb34daa91e3340e1c91869e2ad27c389e5
raymarkcanero <raymarkcanero21@gmail.com>
2026-03-20 19:33:19 +0800

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -74,11 +74,19 @@ document.querySelectorAll('.js-add-to-cart')
       if (matchingItem) {
         matchingItem.quantity += 1;
       } else {
-        cart.push({
-        productId: productId,
-        quantity: 1
-      });
+          cart.push({
+          productId: productId,
+          quantity: 1
+        });
       }
-      console.log(cart);
+
+      let cartQuantity = 0;
+
+      cart.forEach(item => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
     });
 });
