7ad7914e2417b78d8ba005d8ad33a1ddece6b119
raymarkcanero <raymarkcanero21@gmail.com>
2026-03-22 14:59:33 +0800

    Make Payment Summary interactive

--- scripts/checkout/ordersummary.js
+++ scripts/checkout/ordersummary.js
@@ -4,11 +4,7 @@ import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryoption.js';
-hello();
-
-const today = (dayjs());
-const deliveryDate = today.add(7,'days');
-console.log(deliveryDate.format('dddd, MMMM, D'));
+import {renderPaymentSummary} from './paymentsummary.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -132,6 +128,8 @@ export function renderOrderSummary() {
         `.js-cart-item-container-${productId}`
       );
       container.remove();
+
+      renderPaymentSummary();
     });
   });
 
@@ -141,6 +139,7 @@ export function renderOrderSummary() {
       const {productId, deliveryOptionId} = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 }
