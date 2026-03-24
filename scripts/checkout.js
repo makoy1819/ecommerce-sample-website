e7590e6730d66ad24bbf58bfdefb071e33742c47
raymarkcanero <raymarkcanero21@gmail.com>
2026-03-24 12:40:41 +0800

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 import {renderOrderSummary} from './checkout/ordersummary.js';
 import { renderPaymentSummary } from './checkout/paymentsummary.js';
-
+import '../data/cart-oop.js';
 renderOrderSummary();
 renderPaymentSummary();
