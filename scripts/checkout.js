fec26ba9280aa1c32cd4ea651630ed81d620bf05
raymarkcanero <raymarkcanero21@gmail.com>
2026-03-25 11:46:29 +0800

    Create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 import {renderOrderSummary} from './checkout/ordersummary.js';
 import { renderPaymentSummary } from './checkout/paymentsummary.js';
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 renderOrderSummary();
 renderPaymentSummary();
